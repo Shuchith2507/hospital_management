eb82b5f877ef324aa219f30e393e7e6bde5f8192
Shuchith B U <shuchithbu@gmail.com>
2024-01-10 13:57:29 +0530

    adding settings file

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -76,11 +76,21 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospital',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
+
 
 
 # Password validation
